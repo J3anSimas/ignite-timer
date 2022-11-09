3b75416a41614e8e46c93db7cfb5010dc47748e3
Jean Simas <jeansimas@salineira.local>
2022-11-09 17:37:43 -0300

    Font Roboto and Styled-components type def

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { ThemeProvider } from 'styled-components'
+import { Button } from './components/Button/Button.styles'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
 function App(): JSX.Element {
-  return <div className="App">Hello</div>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button />
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
 
 export default App
