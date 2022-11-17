af01bc7e4d0dba6619e32fb3197028486f2b697c
J3anSimas <106534558+J3anSimas@users.noreply.github.com>
2022-11-16 22:49:20 -0300

    Countdown feat (have bugs)

--- src/pages/Home/Home.page.tsx
+++ src/pages/Home/Home.page.tsx
@@ -2,6 +2,7 @@ import { useForm } from 'react-hook-form'
 import { Play } from 'phosphor-react'
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as zod from 'zod'
+import { differenceInSeconds } from 'date-fns'
 import {
   CountdownContainer,
   FormContainer,
@@ -11,12 +12,13 @@ import {
   StartCountdownButton,
   TaskInput
 } from './Home.styles'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 type TCycle = {
   id: string
   task: string
   minutesAmount: number
+  startDate: Date
 }
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
@@ -29,6 +31,7 @@ type TNewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 export default function Home(): JSX.Element {
   const [cycles, setCycles] = useState<TCycle[]>([])
   const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+  const [amountSecondsPast, setAmountSecondsPast] = useState(0)
   const { register, handleSubmit, watch, reset } = useForm<TNewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
@@ -37,19 +40,37 @@ export default function Home(): JSX.Element {
     }
   })
 
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  useEffect(() => {
+    if (activeCycle != null) {
+      const interval = setInterval(() => {
+        setAmountSecondsPast(
+          differenceInSeconds(new Date(), activeCycle.startDate)
+        )
+      }, 1000)
+      return () => clearInterval(interval)
+    }
+  }, [activeCycle])
   function handleCreateNewCycle(data: TNewCycleFormData): void {
     const newCycle: TCycle = {
       id: new Date().getTime().toString(),
       task: data.task,
-      minutesAmount: data.minutesAmount
+      minutesAmount: data.minutesAmount,
+      startDate: new Date()
     }
     setCycles((state) => [...state, newCycle])
     setActiveCycleId(newCycle.id)
     reset()
   }
 
-  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
-  console.log(activeCycle)
+  const totalSeconds = activeCycle != null ? activeCycle.minutesAmount * 60 : 0
+  const currentSeconds =
+    activeCycle != null ? totalSeconds - amountSecondsPast : 0
+
+  const minutesLeft = String(Math.floor(currentSeconds / 60)).padStart(2, '0')
+  const secondsLeft = String(currentSeconds % 60).padStart(2, '0')
+
   const task: string = watch('task')
   const minutesAmount = watch('minutesAmount')
   const isSubmitDisabled: boolean = task === '' || isNaN(minutesAmount)
@@ -90,11 +111,11 @@ export default function Home(): JSX.Element {
           <span>minutes.</span>
         </FormContainer>
         <CountdownContainer>
-          <span>0</span>
-          <span>0</span>
+          <span>{minutesLeft[0]}</span>
+          <span>{minutesLeft[1]}</span>
           <Separator>:</Separator>
-          <span>0</span>
-          <span>0</span>
+          <span>{secondsLeft[0]}</span>
+          <span>{secondsLeft[1]}</span>
         </CountdownContainer>
         <StartCountdownButton type="submit" disabled={isSubmitDisabled}>
           <Play size={24} />
